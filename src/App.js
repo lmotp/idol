7d12f610c11ced29518d996f4ed09f6bcbc7ed5e
박철순 <lmotp@daum.net>
2021-06-16 23:39:04 +0900

    start main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Container from './components/Container';
 import Timer from './components/Timer';
+import Count from './components/Count';
+import Data from './components/Data';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState();
+  useEffect(() => {
+    axios
+      .get('dummy/song_list.json')
+      .then(({ data }) => {
+        setData(data.songList[2].lyrics);
+        setTitle(data.songList[2].title);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">hi</header>
-      <Timer mm={2} ss={0} />
-    </div>
+    <>
+      <Container>
+        <Timer mm={2} ss={0} />
+        <Count />
+        <Data ly={data} title={title} />
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Data.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Data = ({ ly, title }) => {
+  return (
+    <div>
+      <h1>{ly}</h1>
+      <button>{title}</button>
+    </div>
+  );
+};
+
+export default Data;
